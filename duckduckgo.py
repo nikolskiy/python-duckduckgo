b98e2634af4d757cb1c16ac15b5835749c89b98b
Denis Nikolskiy <fsdvn1@gmail.com>
2019-06-26 22:45:33 +0500

    convert the source to work with python3
    urllib changed in python3 so the original code doesn't work there.

--- duckduckgo.py
+++ duckduckgo.py
@@ -5,15 +5,14 @@
 #
 # See LICENSE for terms of usage, modification and redistribution.
 
-import urllib
-import urllib2
+from urllib import request, parse
 import json as j
 import sys
 
 __version__ = 0.242
 
 
-def query(query, useragent='python-duckduckgo '+str(__version__), safesearch=True, html=False, meanings=True, **kwargs):
+def query(qstr, safesearch=True, html=False, meanings=True, **kwargs):
     """
     Query DuckDuckGo, returning a Results object.
 
@@ -33,13 +32,13 @@ def query(query, useragent='python-duckduckgo '+str(__version__), safesearch=Tru
     html: True to allow HTML in output. Default: False (bool)
     meanings: True to include disambiguations in results (bool)
     Any other keyword arguments are passed directly to DuckDuckGo as URL params.
-    """ % __version__
+    """
 
     safesearch = '1' if safesearch else '-1'
     html = '0' if html else '1'
     meanings = '0' if meanings else '1'
     params = {
-        'q': query,
+        'q': qstr,
         'o': 'json',
         'kp': safesearch,
         'no_redirect': '1',
@@ -47,13 +46,11 @@ def query(query, useragent='python-duckduckgo '+str(__version__), safesearch=Tru
         'd': meanings,
         }
     params.update(kwargs)
-    encparams = urllib.urlencode(params)
+    encparams = parse.urlencode(params)
     url = 'http://api.duckduckgo.com/?' + encparams
 
-    request = urllib2.Request(url, headers={'User-Agent': useragent})
-    response = urllib2.urlopen(request)
-    json = j.loads(response.read())
-    response.close()
+    req = request.urlopen(url)
+    json = j.loads(req.read())
 
     return Results(json)
 
@@ -63,23 +60,25 @@ class Results(object):
     def __init__(self, json):
         self.type = {'A': 'answer', 'D': 'disambiguation',
                      'C': 'category', 'N': 'name',
-                     'E': 'exclusive', '': 'nothing'}.get(json.get('Type',''), '')
+                     'E': 'exclusive', '': 'nothing'}.get(
+            json.get('Type', ''), ''
+        )
 
         self.json = json
-        self.api_version = None # compat
+        self.api_version = None  # compat
 
         self.heading = json.get('Heading', '')
 
         self.results = [Result(elem) for elem in json.get('Results',[])]
         self.related = [Result(elem) for elem in
-                        json.get('RelatedTopics',[])]
+                        json.get('RelatedTopics', [])]
 
         self.abstract = Abstract(json)
         self.redirect = Redirect(json)
         self.definition = Definition(json)
         self.answer = Answer(json)
 
-        self.image = Image({'Result':json.get('Image','')})
+        self.image = Image({'Result': json.get('Image', '')})
 
 
 class Abstract(object):
@@ -90,10 +89,12 @@ class Abstract(object):
         self.url = json.get('AbstractURL', '')
         self.source = json.get('AbstractSource')
 
+
 class Redirect(object):
 
     def __init__(self, json):
         self.url = json.get('Redirect', '')
+
 
 class Result(object):
 
@@ -127,6 +128,7 @@ class Answer(object):
         self.text = json.get('Answer')
         self.type = json.get('AnswerType', '')
 
+
 class Definition(object):
     def __init__(self, json):
         self.text = json.get('Definition','')
@@ -134,12 +136,21 @@ class Definition(object):
         self.source = json.get('DefinitionSource')
 
 
-def get_zci(q, web_fallback=True, priority=['answer', 'abstract', 'related.0', 'definition'], urls=True, **kwargs):
-    '''A helper method to get a single (and hopefully the best) ZCI result.
+def get_zci(q, web_fallback=True, priority=('answer', 'definition', 'abstract', 'related.0'), urls=True, **kwargs):
+    """
+    A helper method to get a single (and hopefully the best) ZCI result.
     priority=list can be used to set the order in which fields will be checked for answers.
     Use web_fallback=True to fall back to grabbing the first web result.
-    passed to query. This method will fall back to 'Sorry, no results.' 
-    if it cannot find anything.'''
+    passed to query. This method will fall back to 'Sorry, no results.'
+    if it cannot find anything.
+
+    :param q:
+    :param web_fallback:
+    :param priority:
+    :param urls:
+    :param kwargs:
+    :return:
+    """
 
     ddg = query('\\'+q, **kwargs)
     response = ''
@@ -151,14 +162,20 @@ def get_zci(q, web_fallback=True, priority=['answer', 'abstract', 'related.0', '
 
         result = getattr(ddg, type)
         if index is not None: 
-            if not hasattr(result, '__getitem__'): raise TypeError('%s field is not indexable' % type)
-            result = result[index] if len(result) > index else None
-        if not result: continue
+            if not hasattr(result, '__getitem__'):
+                raise TypeError('%s field is not indexable' % type)
 
-        if result.text: response = result.text
-        if result.text and hasattr(result,'url') and urls: 
-            if result.url: response += ' (%s)' % result.url
-        if response: break
+            result = result[index] if len(result) > index else None
+        if not result:
+            continue
+
+        if result.text:
+            response = result.text
+        if result.text and hasattr(result, 'url') and urls:
+            if result.url:
+                response += ' (%s)' % result.url
+        if response:
+            break
 
     # if there still isn't anything, try to get the first web result
     if not response and web_fallback:
@@ -171,16 +188,30 @@ def get_zci(q, web_fallback=True, priority=['answer', 'abstract', 'related.0', '
 
     return response
 
+
+def show_all(qstr):
+    q = query(qstr)
+    for key in sorted(q.json.keys()):
+        sys.stdout.write(key)
+        if type(q.json[key]) in [str, int]:
+            print(':', q.json[key])
+        else:
+            sys.stdout.write('\n')
+            for i in q.json[key]:
+                print('\t', i)
+
+
+def answer(qstr):
+    response = get_zci(qstr)
+    print(response)
+
+
 def main():
     if len(sys.argv) > 1:
-        q = query(' '.join(sys.argv[1:]))
-        keys = q.json.keys()
-        keys.sort()
-        for key in keys:
-            sys.stdout.write(key)
-            if type(q.json[key]) in [str,unicode,int]: print(':', q.json[key])
-            else: 
-                sys.stdout.write('\n')
-                for i in q.json[key]: print('\t',i)
+        answer(' '.join(sys.argv[1:]))
     else:
         print('Usage: %s [query]' % sys.argv[0])
+
+
+if __name__ == '__main__':
+    main()
