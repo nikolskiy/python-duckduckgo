424013a142f6319c0fb789f94d8b56d055c971e6
Denis Nikolskiy <fsdvn1@gmail.com>
2019-07-06 03:00:26 +0500

    first pass on converting response to dataclass

--- duckduckgo.py
+++ duckduckgo.py
@@ -14,7 +14,6 @@ import sys
 
 from dataclasses import dataclass
 from marshmallow import Schema, fields, post_load
-from pprint import pprint
 
 
 def query(qstr, safe_search=True, html=False, meanings=True, **kwargs):
@@ -55,7 +54,6 @@ def query(qstr, safe_search=True, html=False, meanings=True, **kwargs):
     response = request.urlopen(url)
 
     data = response.read()
-    #pprint(json.loads(data))
 
     schema = ResponseSchema()
     obj = schema.loads(data, partial=True)
@@ -89,30 +87,29 @@ class Image:
     url: str
     height: int
     width: int
+    is_logo: bool
+
+
+@dataclass
+class Icon:
+    url: str
+    height: int
+    width: int
 
 
 @dataclass
 class Topic:
-    url: str
-    result: str
-    text: str
-    icon: str
-
-
-@dataclass
-class Result:
-    topics: List[Topic]
     html: str
     text: str
+    icon: Icon
     url: str
-    icon: str
 
 
 @dataclass
 class Response:
     kind: str
     heading: str
-    results: List[Result]
+    results: List[Topic]
     related_topics: List[Topic]
     abstract: Abstract
     redirect: str
@@ -137,16 +134,16 @@ class SizeInteger(fields.Integer):
 
 
 class IconSchema(Schema):
-    height = SizeInteger(data_key='Height')
     url = fields.Str(data_key='URL')
+    height = SizeInteger(data_key='Height')
     width = SizeInteger(data_key='Width')
 
 
 class TopicSchema(Schema):
-    url = fields.Str(data_key='FirstURL')
+    html = fields.Str(data_key='Result')
     text = fields.Str(data_key='Text')
-    result = fields.Str(data_key='Result')
     icon = fields.Nested(IconSchema, data_key='Icon')
+    url = fields.Str(data_key='FirstURL')
 
 
 class ResponseSchema(Schema):
@@ -173,16 +170,35 @@ class ResponseSchema(Schema):
     meta = fields.Dict(data_key='meta')
 
     @post_load
-    def make_response_class(self, data):
+    def make_response_class(self, data, **kwargs):
         abstract = Abstract(
             html=data['abstract'], text=data['abstract_text'],
             url=data['abstract_url'], source=data['abstract_source']
         )
-        answer = Answer(text=data['answer'], kind=data['answer_type'])
         kind = {'A': 'answer', 'D': 'disambiguation', 'C': 'category', 'N': 'name',
                 'E': 'exclusive', '': 'nothing'}[data['kind']]
-        results = [Result(i[])]
-
+        definition = Definition(
+            text=data['definition'], url=data['definition_url'], source=data['definition_source']
+        )
+        image = Image(
+            url=data['image'], width=data['image_width'],
+            height=data['image_height'], is_logo=data['image_is_logo']
+        )
+        results = [
+            Topic(html=i['html'], text=i['text'], icon=Icon(**i['icon']), url=i['url'])
+            for i in data['results']
+        ]
+        related_topics = [
+            Topic(html=i['html'], text=i['text'], icon=Icon(**i['icon']), url=i['url'])
+            for i in data['related_topics']
+        ]
+        # return data
+        return Response(
+            kind=kind, heading=data['heading'], results=results, related_topics=related_topics,
+            abstract=abstract, redirect=data['redirect'], definition=definition,
+            answer=Answer(text=data['answer'], kind=data['answer_type']),
+            image=image
+        )
 
 
 def get_zci(q, web_fallback=True, priority=('answer', 'definition', 'abstract', 'related.0'), urls=True, **kwargs):
