8d921e853a41959bdb1dffc3806deb77b186999f
Denis <fsdvn1@gmail.com>
2019-08-16 11:39:06 -0700

    Snapcraft build description was added.
    This is the first version that was published to Ubuntu snap store.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import sys
 
 
-MIN_PYTHON = (3, 7)
+MIN_PYTHON = (3, 6)
 if sys.version_info < MIN_PYTHON:
     sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
 
@@ -11,11 +11,16 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 
-VERSION = '0.0.2'
+VERSION = '0.1'
+
+requirements = ['marshmallow>=3.0.0rc7']
+# make sure we have dataclasses
+if sys.version_info < (3, 7):
+    requirements += ['dataclasses']
 
 
 setup(
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     name='duckduckgoapi',
     version=VERSION,
     py_modules=['duckduckgo'],
@@ -25,7 +30,7 @@ setup(
     url='http://github.com/nikolskiy/python-duckduckgo/',
     long_description=long_description,
     platforms=['any'],
-    install_requires=['marshmallow>=3.0.0rc7'],
+    install_requires=requirements,
     keywords='duckduckgo quick answer',
     classifiers=[
         "Development Status :: Alpha",
@@ -35,4 +40,4 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
     ],
-    entry_points={'console_scripts': ['ddg = duckduckgo:main']},)
+    entry_points={'console_scripts': ['dda = duckduckgo:main']},)
